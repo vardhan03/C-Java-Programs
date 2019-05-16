e569182b747fe2a2a882320f3a2fb4562fbe8a0c
Grandhe Harshavardhan <43950124+vardhan03@users.noreply.github.com>
2019-05-16 12:01:21 +0530

    Add files via upload

---
+++ 4negpos.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+void swap(int*a ,int* b)
+{
+ int temp=*a;
+  *a=*b;
+  *b=temp;
+}
+int main()
+{
+ int n,a[100];
+ scanf("%d",&n);
+ for(int i=0;i<n;i++)
+ {
+  scanf("%d",&a[i]);
+ }
+ int j;
+ for(int i=0;i<n-1;i++)
+ {
+  if(a[i]>0 && a[i+1]<0)
+  { 
+  int key=a[i+1];
+   for(j=i;a[j]>0;j--)
+   {
+    a[j+1]=a[j];
+   }
+   a[j+1]=key;
+  }
+ }
+ for(int i=0;i<n;i++)
+ {
+ printf("%d ",a[i]);
+ }
+  return 0;
+ }
